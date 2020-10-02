4931ba73a2ad198228ebdc62191e5af55182b0bf
Lucas Rodrigues Medeiros <lucas.medeiros@radixeng.com.br>
2020-10-02 13:35:39 -0300

    Começando do zero com .Net Core

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace FirstProject
+namespace dotNet
 {
     public class Program
     {
--- README.md
+++
@@ -1,6 +0,0 @@
-dotnet new razor -0 "ProjectName"
-
-
-Executar a aplicação: 
-
--dotnet run => ele busca o arquivo Program.cs ( tem que ta na raiz do projeto )
